4f10ad90301dfd140514d10ce933aa80a5381f29
Jongyun Ha <70758906+JongyunHa@users.noreply.github.com>
2021-08-13 18:16:08 +0900

    Search function refactoring

--- src/app.jsx
+++ src/app.jsx
@@ -2,42 +2,18 @@ import React, { useCallback, useEffect, useState } from 'react';
 import VideoList from './components/video_list';
 import SearchHeader from './components/search_header';
 import styles from './app.module.css';
+import Youtube from './service/youtube';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
-  const search = (query) => {
-    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`;
-
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-
-    fetch(url)
-      .then((response) => response.json())
-      .then((result) =>
-        Array.from(result.items).map((item) => ({
-          ...item,
-          id: item.id.videoId,
-        })),
-      )
-      .then((result) => setVideos(result))
-      .catch((error) => console.log('error', error));
+  const search = async (query) => {
+    const data = await youtube.search(query);
+    setVideos(data);
   };
 
-  const getYoutubeVideos = useCallback(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-
-    fetch(
-      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`,
-      requestOptions,
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log('error', error));
+  const getYoutubeVideos = useCallback(async () => {
+    const data = await youtube.mostPopular();
+    setVideos(data);
   }, []);
 
   useEffect(() => {
